a6b9af3473656f7713312a68b52d86386531acad
PustynnikovKirill <kirillputynnikov1@gmail.com>
2023-07-05 13:16:24 +0300

    homework 3, code-style

--- task1.js
+++ task1.js
@@ -1,10 +1,11 @@
 const deleteElementFromArray = (arr, elem) => {
-    let foundElem = false
-    return arr.filter((el) => {
-        if (el === elem && !foundElem) {
-            foundElem = true
-            return false
-        }
-        return true
-    })
-}
+  let foundElem = false;
+
+  return arr.filter((el) => {
+    if (el === elem && !foundElem) {
+      foundElem = true;
+      return false;
+    }
+    return true;
+  });
+};
