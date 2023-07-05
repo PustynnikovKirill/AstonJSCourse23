56c47ec937ba31f4350b8b100fee0c104445b432
PustynnikovKirill <kirillputynnikov1@gmail.com>
2023-07-05 13:07:37 +0300

    homework 2, code-style

--- task1.js
+++ task1.js
@@ -1,10 +1,13 @@
 const deepCopyObject = (obj) => {
-    if (typeof obj !== 'object' || obj === null) {
-        return obj
-    }
-    const copy = Array.isArray(obj) ? [] : {}
-    Object.keys(obj).forEach((key) => {
-        copy[key] = deepCopyObject(obj[key])
-    })
-    return copy
-}
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  const copy = Array.isArray(obj) ? [] : {};
+
+  Object.keys(obj).forEach((key) => {
+    copy[key] = deepCopyObject(obj[key]);
+  });
+
+  return copy;
+};
